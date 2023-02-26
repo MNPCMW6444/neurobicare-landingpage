bbc5c76b37d973d17461eabf4d14d95a91aab987
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2023-02-26 23:00:57 +0200

    awd

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,106 +1,68 @@
-(function () {
-  "use strict";
+window.onload = function () {
 
-  // ======= Sticky
-  window.onscroll = function () {
-    const ud_header = document.querySelector(".ud-header");
-    const sticky = ud_header.offsetTop;
-    const logo = document.querySelector(".header-logo");
 
-    if (window.pageYOffset > sticky) {
-      ud_header.classList.add("sticky");
-    } else {
-      ud_header.classList.remove("sticky");
+    let dots = document.querySelectorAll(".dot");
+
+    window.setActiveDot = (sectionId) => {
+        if (sectionId == "" || !sectionId) return;
+        dots.forEach(dot => dot.classList.remove("active"));
+        document.querySelector(`.dot[data-section="#${sectionId}"]`).classList.add("active");
     }
 
-    // === logo change
-    if (ud_header.classList.contains("sticky")) {
-      logo.src = "assets/images/logo/logo.png";
-    } else {
-      logo.src = "assets/images/logo/logo-white.png";
-    }
 
-    // show or hide the back-top-top button
-    const backToTop = document.querySelector(".back-to-top");
-    if (
-      document.body.scrollTop > 50 ||
-      document.documentElement.scrollTop > 50
-    ) {
-      backToTop.style.display = "flex";
-    } else {
-      backToTop.style.display = "none";
-    }
-  };
 
-  // ===== responsive navbar
-  let navbarToggler = document.querySelector("#navbarToggler");
-  const navbarCollapse = document.querySelector("#navbarCollapse");
+    let loader = document.getElementsByClassName('loader')[0];
+    let pageContent = document.getElementsByClassName('page-content')[0];
+    var wait = 1000;
 
-  navbarToggler.addEventListener("click", () => {
-    navbarToggler.classList.toggle("navbarTogglerActive");
-    navbarCollapse.classList.toggle("hidden");
-  });
+    
+    setTimeout(() => {
+        loader.style.opacity = 0;
+        pageContent.style.display = 'block';
+        setTimeout(() => {
+            pageContent.style.opacity = 1
+            loader.style.display = 'none'
+            // document.querySelector(`#goal`).scrollIntoView({
+            //     behavior: "smooth",
+            //     block: "start",
+            //     inline: "nearest"
+            // });
+        }, wait / 2);
+    }, wait)
 
-  //===== close navbar-collapse when a  clicked
-  document
-    .querySelectorAll("#navbarCollapse ul li:not(.submenu-item) a")
-    .forEach((e) =>
-      e.addEventListener("click", () => {
-        navbarToggler.classList.remove("navbarTogglerActive");
-        navbarCollapse.classList.add("hidden");
-      })
-    );
 
-  // ===== Sub-menu
-  const submenuItems = document.querySelectorAll(".submenu-item");
-  submenuItems.forEach((el) => {
-    el.querySelector("a").addEventListener("click", () => {
-      el.querySelector(".submenu").classList.toggle("hidden");
+
+
+    setActiveDot("home");
+
+
+    dots.forEach(dot => {
+        dot.addEventListener("click", () => {
+            dots.forEach(dot => dot.classList.remove("active"));
+            dot.classList.add("active");
+            document.querySelector(dot.dataset.section).scrollIntoView({
+                behavior: "smooth",
+                block: "start",
+                inline: "nearest"
+            });
+        });
     });
-  });
 
-  // ===== Faq accordion
-  const faqs = document.querySelectorAll(".single-faq");
-  faqs.forEach((el) => {
-    el.querySelector(".faq-btn").addEventListener("click", () => {
-      el.querySelector(".icon").classList.toggle("rotate-180");
-      el.querySelector(".faq-content").classList.toggle("hidden");
+
+
+
+    window.addEventListener('scroll', function () {
+        console.log('scroll 1');
+    })
+
+    document.body.addEventListener('scroll', () => {
+        const sections = document.querySelectorAll("section");
+        sections.forEach(section => {
+            const rect = section.getBoundingClientRect();
+            if (rect.top >= 0 && rect.top <= 110) {
+                setActiveDot(section.id);
+            }
+        });
     });
-  });
+}
 
-  // ===== wow js
-  new WOW().init();
-
-  // ====== scroll top js
-  function scrollTo(element, to = 0, duration = 500) {
-    const start = element.scrollTop;
-    const change = to - start;
-    const increment = 20;
-    let currentTime = 0;
-
-    const animateScroll = () => {
-      currentTime += increment;
-
-      const val = Math.easeInOutQuad(currentTime, start, change, duration);
-
-      element.scrollTop = val;
-
-      if (currentTime < duration) {
-        setTimeout(animateScroll, increment);
-      }
-    };
-
-    animateScroll();
-  }
-
-  Math.easeInOutQuad = function (t, b, c, d) {
-    t /= d / 2;
-    if (t < 1) return (c / 2) * t * t + b;
-    t--;
-    return (-c / 2) * (t * (t - 2) - 1) + b;
-  };
-
-  document.querySelector(".back-to-top").onclick = () => {
-    scrollTo(document.documentElement);
-  };
-})();
